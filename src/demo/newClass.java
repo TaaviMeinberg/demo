8cbd6c7d2a92bc88532522baf00fab4a4de8b91e
Taavi Meinberg <Taavi Meinberg@A2>
2016-09-27 14:15:37 +0300

    Added new class "newClass"

---
+++ src/demo/newClass.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class newClass {
+
+	public static void main(String[] args) {
+		int a = 5;
+		//aisufbauibf sdgnafn iuasbgiabfjasf iuabf iuabsf adbijfgvdwbjitg3euobsjftc saiubgfffffff
+	}
+
+}
