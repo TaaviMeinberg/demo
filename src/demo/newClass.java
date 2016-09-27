d0d0ae8604573f2f5f5c64a38d2c18a580da97ac
TaaviMeinberg <taavi.meinberg@gmail.com>
2016-09-27 14:21:56 +0300

    Update newClass.java

--- src/demo/newClass.java
+++ src/demo/newClass.java
@@ -3,7 +3,7 @@ package demo;
 public class newClass {
 
 	public static void main(String[] args) {
-		int a = 5;
+		int b = 55;
 		//aisufbauibf sdgnafn iuasbgiabfjasf iuabf iuabsf adbijfgvdwbjitg3euobsjftc saiubgfffffff
 	}
 
